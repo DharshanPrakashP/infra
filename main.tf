dddf94609e7896825c210980ef5006a7993155d2
DharshanPrakashP <153717708+DharshanPrakashP@users.noreply.github.com>
2023-12-24 23:47:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -59,7 +59,7 @@ resource "aws_security_group" "Test_secgrp" {
 
 // EC2 Instance
 resource "aws_instance" "Test-server" {
-  ami                    = "ami-0416c18e75bd69567"
+  ami                    = "ami-079db87dc4c10ac91"
   key_name               = "Test"
   instance_type          = "t3.micro"
   subnet_id              = aws_subnet.Test_Subnet.id
